346e0ec753f110a7d8c95da53315eef2ea5497df
Futurematic <darkice@web.de>
2025-04-20 20:26:43 +0200

    First check in

---
+++ examples/echo_client_std.rs
@@ -0,0 +1,9 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+const ECHO_SERVER_ADDRESS: &str = "localhost:1234";
+
+fn main(){
+    //connection
+    println!("connecting to {}", ECHO_SERVER_ADDRESS);
+}
